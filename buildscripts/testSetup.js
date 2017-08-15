6d55370fb775b2a50dd388a431c4f066ae6415a5
Cook <BRANDON_COOK@homedepot.com>
2017-08-15 16:32:21 -0400

    babel transpilling fix

--- buildscripts/testSetup.js
+++ buildscripts/testSetup.js
@@ -1,7 +1,12 @@
 // This file isn't transpiled, so must use CommonJS and ES5
 
 // Register babel to transpile before out tests run.
-require('babel-register')();
+//require('babel-register')();
+const register = require('babel-register');
+
+register({
+    ignore: /node_modules\/(?!ptz-core-domain)/,
+});
 
 // Disable webpack feature that Mocha doesn't understand.
 require.extensions['.css'] = function() {};
